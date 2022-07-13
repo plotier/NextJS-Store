35dcca2fd80e74ac937759ef63b8189a2801774a
plotier <plotier15@gmail.com>
2022-07-13 16:41:17 +0200

    before the first component, fixed

---
+++ pages/faq.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import Link from 'next/link'
+
+export const faq = () => {
+  return (
+    <div>
+        <h1>FAQ</h1>
+        <p>Hola 
+        <Link href="/">
+  <a>Ir a Home</a>
+</Link>
+
+        </p>
+
+    </div>
+  )
+}
+
+export default faq
---
+++ pages/store/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const index = () => {
+  return (
+    <div>
+        <h1>Store</h1>
+    </div>
+  )
+}
+
+export default index
