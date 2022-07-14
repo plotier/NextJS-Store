6460382e3cfaf704f66810fe7811bfa17c8a021e
plotier <82114919+plotier@users.noreply.github.com>
2022-07-14 11:50:49 +0200

    working on dynamic routes

---
+++ pages/store/[id].js
@@ -0,0 +1,18 @@
+import Layout from "../../components/layout";
+import Product from "../../components/product";
+import { getPathsFromIds } from "../../lib/utils";
+
+export default function ProductPage (){
+    return( <Layout>
+        <Product />
+    </Layout>);
+}
+
+ export async function getStaticPaths(){
+    const paths = await getPathsFromIds();
+
+    return{
+        paths:paths,
+        fallback:false
+    }
+ }
